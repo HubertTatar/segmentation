c55e09ddee20115f3a0858564c4007fa89eb6d60
HubertTatar <hutatar@gmail.com>
2020-08-30 20:42:10 +0200

    use repository

--- src/main/kotlin/io/huta/segmentation/Main.kt
+++ src/main/kotlin/io/huta/segmentation/Main.kt
@@ -1,8 +1,11 @@
 package io.huta.segmentation
 
+import arrow.core.Either
+import arrow.fx.extensions.io.applicativeError.attempt
 import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.*
 import io.ktor.features.*
+import io.ktor.http.*
 import io.ktor.jackson.*
 import io.ktor.response.*
 import io.ktor.routing.*
@@ -11,7 +14,7 @@ import io.ktor.server.netty.*
 
 fun main() {
 
-    //val repository = IOPlayerRepository()
+    val repository = IOPlayerRepository()
 
     val server = embeddedServer(Netty, port = 8080) {
         install(ContentNegotiation) {
@@ -21,7 +24,10 @@ fun main() {
         }
         install(Routing) {
             get("/players") {
-                call.respond(listOf(Player(1, "john", "doe")))
+                when (val result = repository.findAll().attempt().unsafeRunSync()) {
+                    is Either.Right -> call.respond(result.b)
+                    else -> call.respond(HttpStatusCode.InternalServerError)
+                }
             }
         }
     }
--- src/main/kotlin/io/huta/segmentation/PlayerRepository.kt
+++ src/main/kotlin/io/huta/segmentation/PlayerRepository.kt
@@ -3,13 +3,14 @@ package io.huta.segmentation
 import arrow.Kind
 import arrow.fx.ForIO
 import arrow.fx.IO
+import arrow.fx.extensions.fx
 
 interface PlayerRepository<F> {
     fun findAll(): Kind<F, List<Player>>
 }
 
 class IOPlayerRepository: PlayerRepository<ForIO> {
-    override fun findAll(): Kind<ForIO, List<Player>> = IO {
+    override fun findAll(): Kind<ForIO, List<Player>> = IO.fx {
         listOf(Player(1, "john", "doe"))
     }
 }
